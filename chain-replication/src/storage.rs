1ce6f07eec97a797d7122f8a83f932fccbea479e
Zack Owens <zowens2009@gmail.com>
2024-01-23 08:12:56 -0800

    WIP

--- chain-replication/src/storage.rs
+++ chain-replication/src/storage.rs
@@ -1,5 +1,9 @@
 use crate::{Buffer, Entry, Key, KeyedEntry, Slot};
 use std::{error::Error, fmt::Debug, future::Future};
+use std::marker::PhantomData;
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::collections::VecDeque;
+use std::io::IoSlice;
 
 /// Abstraction for persistent storage for chain replicaiton. The Storage must
 /// be able to linearly request updates, append operations, and lookup entries
@@ -33,4 +37,122 @@ pub trait KeyedStorage<K: Key, E: KeyedEntry<Key = K>>: Storage<E> {
     type EntryFuture: Future<Output = Result<Option<(Slot, E)>, Self::Error>>;
     /// Grabs the latest slot and value for a key.
     fn entry(&self, key: K) -> Self::EntryFuture;
+}
+
+
+
+
+
+/// Storage implementation that buffers entries in memory while replication is waiting.
+pub struct BufferedStorage<E: Entry, S: Storage<E>> {
+    storage: S,
+    _e: PhantomData<E>,
+}
+
+struct ChainedBuf<B: Buf> {
+    buffers: VecDeque<B>,
+}
+
+impl<B: Buf> Buf for ChainedBuf<B> {
+
+    fn remaining(&self) -> usize {
+        self.buffers.iter().map(|b| b.remaining()).sum()
+    }
+
+    fn chunk(&self) -> &[u8] {
+        for b in self.buffers.iter() {
+            if b.has_remaining() {
+                return b.chunk()
+            }
+        }
+        return &[];
+    }
+
+    fn advance(&mut self, mut cnt: usize) {
+        loop {
+            match self.buffers.front_mut() {
+                Some(mut v) if v.has_remaining() => {
+                    let v_rem = v.remaining();
+                    if v_rem >= cnt {
+                        v.advance(cnt);
+                        return;
+                    }
+                    v.advance(v_rem);
+                    cnt -= v_rem;
+                }
+                None => return,
+                _ => {
+                    // remove empty buffer without calling advance
+                }
+            }
+
+            // if we get to this branch, pop the front value (does not have any remaining itmes)
+            self.buffers.pop_front();
+        }
+    }
+
+    fn chunks_vectored<'a>(&'a self, dst: &mut [IoSlice<'a>]) -> usize {
+        let mut n = 0;
+        for b in self.buffers.iter() {
+            n += b.chunks_vectored(&mut dst[n..]);
+            if n >= dst.len() {
+                break;
+            }
+        }
+
+        n
+    }
+
+    fn copy_to_bytes(&mut self, len: usize) -> Bytes {
+        // pop empty buffers
+        loop {
+            match self.buffers.front() {
+                Some(v) if v.has_remaining() => break,
+                None => return Bytes::from_static(&[]),
+                _ => {
+                    // fallthrough to pop the buffer
+                }
+            }
+            self.buffers.pop_front();
+        }
+
+        // fast path: the first buffer can be coppied
+        {
+            let buf_len = self.buffers.len();
+            let front = self.buffers.front_mut().unwrap();
+            if buf_len == 1 || front.remaining() <= len {
+                return front.copy_to_bytes(len);
+            }
+        }
+
+        let mut ret = BytesMut::with_capacity(len);
+        let mut written = 0;
+        for mut b in self.buffers.iter_mut() {
+            let b_rem = b.remaining();
+            if b_rem + written < len {
+                ret.put(&mut b);
+                written += b_rem;
+            } else {
+                ret.put((&mut b).take(len - b_rem));
+                break;
+            }
+        }
+        return ret.freeze();
+
+        /*let a_rem = self.a.remaining();
+        if a_rem >= len {
+            self.a.copy_to_bytes(len)
+        } else if a_rem == 0 {
+            self.b.copy_to_bytes(len)
+        } else {
+            assert!(
+                len - a_rem <= self.b.remaining(),
+                "`len` greater than remaining"
+            );
+            let mut ret = crate::BytesMut::with_capacity(len);
+            ret.put(&mut self.a);
+            ret.put((&mut self.b).take(len - a_rem));
+            ret.freeze()
+        }*/
+    }
 }
